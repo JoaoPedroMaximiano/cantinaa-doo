baaff6f291d107fb02bcec7116d31f9801c7f4bf
Douglas Guimarães dos Santos <douglasguimaaraes@gmail.com>
2023-09-21 20:15:39 -0300

    Create preview Services

---
+++ CantinaIFSC/src/service/CaixaService.java
@@ -0,0 +1,31 @@
+package service;
+
+import model.bo.Caixa;
+
+public class CaixaService {
+
+    public void adicionar(Caixa objeto){
+        new CaixaDAO().create(objeto);
+    }
+
+    public static List<Caixa> carregar(){
+        return new CaixaDAO().retrive();
+    }
+
+    public static Caixa carregar(int id){
+        return new CaixaDAO().retrive(id);
+    }
+
+    public static List<Caixa> carregar(Caixa filtro){
+        return new CaixaDAO().retrive(filtro);
+    }
+    
+    public void atualizar(Caixa objeto){
+        new CaixaDAO().update(objeto);
+    }
+    
+    public void deletar(Caixa objeto){
+        new CaixaDAO().delete(objeto);
+    }    
+    
+}
---
+++ CantinaIFSC/src/service/CidadeService.java
@@ -0,0 +1,31 @@
+package service;
+
+import model.bo.Cidade;
+
+public class CidadeService {
+    
+    public void adicionar(Cidade objeto){
+        new CidadeDAO().create(objeto);
+    }
+
+    public static List<Cidade> carregar(){
+        return new CidadeDAO().retrive();
+    }
+
+    public static Cidade carregar(int id){
+        return new CidadeDAO().retrive(id);
+    }
+
+    public static List<Cidade> carregar(Cidade filtro){
+        return new CidadeDAO().retrive(filtro);
+    }
+    
+    public void atualizar(Cidade objeto){
+        new CidadeDAO().update(objeto);
+    }
+    
+    public void deletar(Cidade objeto){
+        new CidadeDAO().delete(objeto);
+    }    
+    
+}
